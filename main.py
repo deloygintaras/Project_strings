51184ad1edf574c7d33afe171485c76372cdd790
deloygintaras <turcinasgintaras@gmail.com>
2024-09-26 21:18:06 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# 1. Sukurti du kintamuosius. Jiems priskirti savo mylimo aktoriaus vardą ir pavardę kaip stringus (Jonas Jonaitis).
+# Atspausdinti trumpesnį stringą.
+vardas = "Dwayne"
+pavarde = "Johnson"
+strings = [vardas, pavarde]
+print(min(strings, key=len))
+
